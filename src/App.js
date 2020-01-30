1301a2b7f3beb863fa24335f6a7d82ab74208536
Octavio <octavio13.ft@gmail.com>
2020-01-30 14:35:38 +0000

    initial

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    const [todo, setTodo] = React.useState('');
+    const [list, setList] = React.useState([]);
+
+    const onAdd = (e) => {
+        setList([...list, todo]);
+        setTodo('')
+    }
+
+    const onRemove = (e) => {
+        const newList = [...list.filter(x => x !== e)]
+        setList(newList);
+        //// [...list.filter(x=> x!== item1)]= [ item2, item3]
+    }
+
+    const onInputChange = (e) => {
+        setTodo(e.target.value)
+    }
+
+    console.log('I am redering')
+
+    return <div style={{ margin: 10 }}>
+        <div style={{ paddingBottom: 10 }}>Todo</div>
+        <div>
+            <label>
+                Todo:
+    <input type="text" name="todo" value={todo} style={{ marginLeft: 5 }} onChange={onInputChange} />
+            </label>
+            <input type="button" value="Submit" onClick={(x) => onAdd(x)}></input>
+        </div>
+        <div>
+            <ul>
+                {list.map((item, i) => <li  onClick={(x) => onRemove(item)} >{item} <button type="button">mark as Done!</button> <button type="button"> delete</button> </li>   )}
+            </ul>
+        </div>
+
+    </div>
+
+
+}
+
+
+export default App;
+
